7d5072fe720ddf18e0b6004ae0004a7c4a487705
jonathancklee1 <jonathancklee@gmail.com>
2021-12-29 15:29:55 +1100

    Fixed Favourites function

--- index.js
+++ index.js
@@ -22,10 +22,13 @@ const sortDropdown = document.querySelector(".sort-dropdown");
 const favCountryBtn = document.querySelector(".favourites-btn");
 const favCarousel = document.querySelector(".carousel-track");
 
+const date = document.querySelector(".date-text");
 const nameSortAZ = document.getElementById("sort-name-A-Z");
 const nameSortZA = document.getElementById("sort-name-Z-A");
 const caseSortAsc = document.getElementById("sort-case-asc");
 const caseSortDesc = document.getElementById("sort-case-desc");
+
+let noneChecked = true;
 
 nameSortAZ.addEventListener("click", () => {
   sortList("alphaAZ");
@@ -39,6 +42,20 @@ caseSortAsc.addEventListener("click", () => {
 caseSortDesc.addEventListener("click", () => {
   sortList("caseDesc");
 });
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+  checkIfFavEmpty();
+});
+
+function checkIfFavEmpty() {
+  const favCountryItem = document.querySelector(".fav-country-item");
+  if (favCarousel.contains(favCountryItem)) {
+    const noFavText = document.getElementById("no-fav-text");
+    noFavText.remove();
+    console.log("removed");
+  }
+}
 
 // Sort list of countries based on which sort type is pressed
 function sortList(type) {
@@ -124,26 +141,21 @@ function search() {
 
 favCountryBtn.addEventListener("click", () => {
   getChecked();
+  if (noneChecked) {
+    alert("No countries selected");
+  }
   getData().then((data) => {
     const favCountryItem = document.querySelectorAll(".fav-country-item");
     favCountryItem.forEach((item) => {
       item.addEventListener("click", (e) => {
         console.log("HI");
-        let favCountryName = e.currentTarget.childNodes[2].innerHTML;
+        let favCountryName = e.target.childNodes[2].innerHTML;
         console.log(favCountryName);
         displayModal(data, favCountryName);
       });
     });
   });
-
-  // for (let k in favCountryArray) {
-  // let li = document.createElement("li");
-  // let h2 = document.createElement("h2");
-  // h2.appendChild(document.createTextNode(`${favCountryArray[k]}`));
-  // li.appendChild(h2);
-  // favCarousel.appendChild(li);
-  // }
-  // console.log(favCountryArray);
+  checkIfFavEmpty();
 });
 
 // Loop through country list to see checked country
@@ -152,30 +164,24 @@ function getChecked() {
   countryItem.forEach((item) => {
     if (item.childNodes[0].checked) {
       let newLi = item.cloneNode(true);
-      // let li = document.createElement("li");
-      // let favCountryFlag = item.childNodes[1].cloneNode(true);
-      // let favCountryName = item.childNodes[2].cloneNode(true);
-      // let favCountryNewCases = item.childNodes[3].cloneNode(true);
-
-      // favCountryName.setAttribute("class", "fav-country-name");
-      // h2.appendChild(
-      //   document.createTextNode(`${item.childNodes[2].innerHTML}`)
-      // );
-      // li.appendChild(favCountryFlag);
-      // li.appendChild(favCountryName);
-      // li.appendChild(favCountryNewCases);
-      // newLi.removeChild(newLi.firstChild);
+      let btn = document.createElement("button");
+      btn.setAttribute("class", "del-country-btn");
+      btn.innerHTML = `<i class="fas fa-times"></i>`;
       newLi.setAttribute("class", "fav-country-item");
-      newLi.childNodes[2].setAttribute("class", "");
-      // newLi.childNodes[0].style.display = "none";
-      favCarousel.appendChild(newLi);
+      newLi.childNodes[2].setAttribute("class", "fav-country-name");
+      newLi.childNodes[0].style.visibility = "hidden";
+      newLi.appendChild(btn);
 
-      // favCountryArray.push(item.childNodes[2].innerHTML);
-      item.childNodes[0].setAttribute("disabled", "disabled");
+      favCarousel.appendChild(newLi);
+      item.childNodes[0].setAttribute("disabled", "true");
       item.childNodes[0].checked = false;
+      noneChecked = false;
+      btn.addEventListener("click", () => {
+        btn.parentElement.remove();
+        item.childNodes[0].removeAttribute("disabled");
+      });
     }
   });
-  console.log(favCountryArray);
 }
 
 function getData() {
@@ -183,16 +189,46 @@ function getData() {
     fetch(covid_url)
       .then((response) => response.json())
       .then((data) => {
-        resolve(data);
+        if (data) {
+          resolve(data);
+        } else {
+          reject("Data is unavailable");
+        }
       });
-    // .catch((error) => alert("Cannot retrieve data"));
   });
 }
 
-// fetch(covid_url)
-//   .then((response) => response.json())
-//   .then((data) => {
 getData().then((data) => {
+  displayCountryList(data);
+
+  const countryItem = document.querySelectorAll(".country-item");
+  countryItem.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      let countryName = e.target.childNodes[2].innerHTML;
+      displayModal(data, countryName);
+    });
+  });
+});
+function displayModal(jData, cName) {
+  // Get clicked country name
+
+  modal.style.display = "block";
+  // Change modal content to clicked country
+  countryNameModal.innerHTML = `${cName}`;
+  for (let j in jData.Countries) {
+    if (cName == jData.Countries[j].Country) {
+      let countryCodeModal = jData.Countries[j].CountryCode;
+      countryFlagModal.src = `https://flagcdn.com/256x192/${countryCodeModal.toLowerCase()}.png`;
+      countryNewCasesModal.innerHTML = `New Cases: +${jData.Countries[j].NewConfirmed}`;
+      countryTotalCasesModal.innerHTML = `Total Cases: ${jData.Countries[j].TotalConfirmed}`;
+      countryNewDeathsModal.innerHTML = `New Deaths: +${jData.Countries[j].NewDeaths}`;
+      countryTotalDeathsModal.innerHTML = `Total Deaths: ${jData.Countries[j].TotalDeaths}`;
+    }
+  }
+}
+
+function displayCountryList(data) {
+  date.innerHTML = `Updated on the ${data.Date.substring(0, 10)}`;
   for (let i in data.Countries) {
     let li = document.createElement("li");
     let h2 = document.createElement("h2");
@@ -217,101 +253,7 @@ getData().then((data) => {
     li.appendChild(p);
     countryList.appendChild(li);
   }
-  const countryItem = document.querySelectorAll(".country-item");
-  countryItem.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      let countryName = e.target.childNodes[2].innerHTML;
-      displayModal(data, countryName);
-    });
-  });
-  // displayModal(data);
-  // Modal Detail
-  // const countryItem = document.querySelectorAll(".country-item");
-  // countryItem.forEach((item) => {
-  //   item.addEventListener("click", (e) => {
-  //     // Get clicked country name
-  //     let countryName = e.target.childNodes[2].innerHTML;
-  //     modal.style.display = "block";
-  //     // Change modal content to clicked country
-  //     countryNameModal.innerHTML = `${countryName}`;
-  //     for (let j in data.Countries) {
-  //       if (countryName == data.Countries[j].Country) {
-  //         let countryCodeModal = data.Countries[j].CountryCode;
-  //         countryFlagModal.src = `https://flagcdn.com/256x192/${countryCodeModal.toLowerCase()}.png`;
-  //         countryNewCasesModal.innerHTML = `New Cases: +${data.Countries[j].NewConfirmed}`;
-  //         countryTotalCasesModal.innerHTML = `Total Cases: ${data.Countries[j].TotalConfirmed}`;
-  //         countryNewDeathsModal.innerHTML = `New Deaths: +${data.Countries[j].NewDeaths}`;
-  //         countryTotalDeathsModal.innerHTML = `Total Deaths: ${data.Countries[j].TotalDeaths}`;
-  //       }
-  //     }
-  //   });
-  // });
-});
-function displayModal(jData, cName) {
-  // Get clicked country name
-
-  modal.style.display = "block";
-  // Change modal content to clicked country
-  countryNameModal.innerHTML = `${cName}`;
-  for (let j in jData.Countries) {
-    if (cName == jData.Countries[j].Country) {
-      let countryCodeModal = jData.Countries[j].CountryCode;
-      countryFlagModal.src = `https://flagcdn.com/256x192/${countryCodeModal.toLowerCase()}.png`;
-      countryNewCasesModal.innerHTML = `New Cases: +${jData.Countries[j].NewConfirmed}`;
-      countryTotalCasesModal.innerHTML = `Total Cases: ${jData.Countries[j].TotalConfirmed}`;
-      countryNewDeathsModal.innerHTML = `New Deaths: +${jData.Countries[j].NewDeaths}`;
-      countryTotalDeathsModal.innerHTML = `Total Deaths: ${jData.Countries[j].TotalDeaths}`;
-    }
-  }
 }
-
-// for (let i in data.Countries) {
-//   let li = document.createElement("li");
-//   let h2 = document.createElement("h2");
-//   let p = document.createElement("p");
-//   let img = document.createElement("img");
-//   let checkbox = document.createElement("INPUT");
-//   checkbox.setAttribute("type", "checkbox");
-//   checkbox.setAttribute("class", "country-cb");
-//   checkbox.value = `${data.Countries[i].Country}`;
-//   let countryCode = data.Countries[i].CountryCode;
-//   h2.appendChild(document.createTextNode(`${data.Countries[i].Country}`));
-//   h2.setAttribute("class", "country-name");
-//   p.appendChild(document.createTextNode(`+${data.Countries[i].NewConfirmed}`));
-//   img.src = `https://flagcdn.com/96x72/${countryCode.toLowerCase()}.png`;
-//   img.setAttribute("class", "country-flag");
-//   li.appendChild(checkbox);
-//   li.appendChild(img);
-//   li.appendChild(h2);
-//   li.setAttribute("class", "country-item");
-//   li.appendChild(p);
-//   countryList.appendChild(li);
-// }
-
-// Modal Detail
-// const countryItem = document.querySelectorAll(".country-item");
-// countryItem.forEach((item) => {
-//   item.addEventListener("click", (e) => {
-//     // Get clicked country name
-//     let countryName = e.target.childNodes[2].innerHTML;
-//     modal.style.display = "block";
-//     // Change modal content to clicked country
-//     countryNameModal.innerHTML = `${countryName}`;
-//     for (let j in data.Countries) {
-//       if (countryName == data.Countries[j].Country) {
-//         let countryCodeModal = data.Countries[j].CountryCode;
-//         countryFlagModal.src = `https://flagcdn.com/256x192/${countryCodeModal.toLowerCase()}.png`;
-//         countryNewCasesModal.innerHTML = `New Cases: +${data.Countries[j].NewConfirmed}`;
-//         countryTotalCasesModal.innerHTML = `Total Cases: ${data.Countries[j].TotalConfirmed}`;
-//         countryNewDeathsModal.innerHTML = `New Deaths: +${data.Countries[j].NewDeaths}`;
-//         countryTotalDeathsModal.innerHTML = `Total Deaths: ${data.Countries[j].TotalDeaths}`;
-//       }
-//     }
-//   });
-// });
-// })
-// .catch((error) => alert("Cannot retrieve data"));
-
 // Modal Close Functions
 window.onclick = (event) => {
   if (event.target == modal) {
